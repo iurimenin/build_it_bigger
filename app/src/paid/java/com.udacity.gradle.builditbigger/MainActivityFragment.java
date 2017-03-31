0246bee4b2e57b8f00303f64c5b8e13f4939b2de
iurimenin <iuri020293@>
2017-03-31 09:48:37 -0300

    Paid and Free versions

--- app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com.udacity.gradle.builditbigger/MainActivityFragment.java
@@ -6,8 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.udacity.gradle.builditbigger.R;
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -19,6 +17,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
